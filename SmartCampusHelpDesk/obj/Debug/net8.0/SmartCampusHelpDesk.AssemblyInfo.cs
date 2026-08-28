8227cfb3f3d24870203bd67ea38214a563e99097
marno <Marno2007dutoit@gmail.com>
2026-08-28 12:03:21 +0200

    LOL

--- SmartCampusHelpDesk/obj/Debug/net8.0/SmartCampusHelpDesk.AssemblyInfo.cs
+++ SmartCampusHelpDesk/obj/Debug/net8.0/SmartCampusHelpDesk.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartCampusHelpDesk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c511bc4042596201aee3b9a664b430b80e961c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e2f182ddf1cb1473208000391dd1277b57ef306")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartCampusHelpDesk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartCampusHelpDesk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

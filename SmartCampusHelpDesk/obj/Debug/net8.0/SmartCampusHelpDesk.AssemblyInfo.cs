f68e921cfc199704eca6c584c0e54d3b1a621e9c
Gregory Candiotes <gregory.candiotes77@gmail.com>
2026-08-28 12:02:52 +0200

    Update assembly info and resolve merge conflicts in editor config

--- SmartCampusHelpDesk/obj/Debug/net8.0/SmartCampusHelpDesk.AssemblyInfo.cs
+++ SmartCampusHelpDesk/obj/Debug/net8.0/SmartCampusHelpDesk.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartCampusHelpDesk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e572d345723c08aa3fc81abaa9687470b3ad6b7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+734afebafda48af5bbe3ba921bae083ae58d52f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartCampusHelpDesk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartCampusHelpDesk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

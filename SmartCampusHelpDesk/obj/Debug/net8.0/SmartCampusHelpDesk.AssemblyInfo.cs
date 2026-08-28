8ccba12f2b20792314e89941e2f0ead0331ab9c2
TiaanSchoeman <ID+TiaanSchoeman@users.noreply.github.com>
2026-08-28 11:05:02 +0200

    Update project paths and configurations for new user environment
    - Changed user-specific paths from 'grego' to 'tscho' in various project files.
    - Updated static web assets and project asset configurations to reflect new directory structure.
    - Adjusted NuGet package paths and versions to align with the new environment.
    - Removed unnecessary download dependencies from project.assets.json.
    - Updated SDK analysis levels and runtime identifier graph paths to match the new setup.

--- SmartCampusHelpDesk/obj/Debug/net8.0/SmartCampusHelpDesk.AssemblyInfo.cs
+++ SmartCampusHelpDesk/obj/Debug/net8.0/SmartCampusHelpDesk.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartCampusHelpDesk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a008a4416e9b58484ecd53aa99fac02477fba37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c659bdb6ee8e973c8f0231f4272f0a22463a8a95")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartCampusHelpDesk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartCampusHelpDesk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
